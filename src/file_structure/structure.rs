f9aef8358dc3181b8e766aa758e71c44f8425fb6
denis <denis@morelfr.ch>
2023-02-19 18:34:47 +0100

    Start file structure (WIP)

---
+++ src/file_structure/structure.rs
@@ -0,0 +1,24 @@
+/*
+pub struct File<T: FromJson> {
+    location: Path,
+    name: Name,
+    data: Option<T>,
+}
+
+pub struct FileGroup<T: FromJson> {
+    location: Path,
+    name_search: Name,
+    data: Option<T>,
+}
+
+pub struct Directory {
+    path: Path,
+    files: Vec<File>,
+    file_groups: Vec<FileGroup>,
+    directories: Vec<Directory>,
+}
+
+impl FileGroup {
+    pub fn get_files() -> Vec<File> {}
+}
+ */
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 pub mod crypto_primitives;
 pub mod data_structures;
 pub mod error;
+pub mod file_structure;
 
 use crypto_primitives::byte_array::ByteArray;
 use num::bigint::{BigUint, ToBigUint};
