1a6e0eb2fc1f9b2499129f0d21e4670a0e047650
De-Mo <denis@morelfr.ch>
2023-12-13 22:06:33 +0000

    Corect direct-trust documentation

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,5 @@
+//! Module containing the contstants and the way to access them
+
 use std::path::{Path, PathBuf};
 
 // Constants from specification
@@ -16,7 +18,7 @@ const BB_DIR_NAME: &str = "ballot_boxes";
 // Program structure
 const LOG_DIR_NAME: &str = "log";
 const LOG_FILE_NAME: &str = "log.txt";
-const DIRECT_TRUST_DIR_NAME: &str = "direct_trust";
+const DIRECT_TRUST_DIR_NAME: &str = "direct-trust";
 //const RESOURCES_DIR_NAME: &str = "resources";
 // const SCHEMA_PATH: &str = "schemas";
 //const VERIFICATION_LIST_FILE_NAME: &str = "verification_list.json";
@@ -49,18 +51,22 @@ impl Config {
         Config(root_dir)
     }
 
+    /// Path of the root directory of the programm
     pub fn root_dir_path(&self) -> PathBuf {
         Path::new(self.0).to_path_buf()
     }
 
+    /// Maximum number of voting options according to the specification
     pub fn maximum_number_of_voting_options() -> usize {
         MAXIMUM_NUMBER_OF_VOTING_OPTIONS
     }
 
+    /// Maximum number of selectable voting options according to the specification
     pub fn maximum_number_of_selectable_voting_options() -> usize {
         MAXIMUM_NUMBER_OF_SELECTABLE_VOTING_OPTIONS
     }
 
+    /// Maximum number of write-in options according to the specification
     pub fn maximum_number_of_write_in_options() -> usize {
         MAXIMUM_NUMBER_OF_WRITE_IN_OPTIONS
     }
@@ -73,30 +79,37 @@ impl Config {
         MAXIMUM_ACTUAL_VOTING_OPTION_LENGTH
     }
 
+    /// The name of the setup directory
     pub fn setup_dir_name() -> &'static str {
         SETUP_DIR_NAME
     }
 
+    /// The name of the tally directory
     pub fn tally_dir_name() -> &'static str {
         TALLY_DIR_NAME
     }
 
+    /// The name of the vcs (voting card sets) directories
     pub fn vcs_dir_name() -> &'static str {
         VCS_DIR_NAME
     }
 
+    /// The name of the bb (ballot boxes) directories
     pub fn bb_dir_name() -> &'static str {
         BB_DIR_NAME
     }
 
+    /// The path to the log file
     pub fn log_file_path(&self) -> PathBuf {
         self.root_dir_path().join(LOG_DIR_NAME).join(LOG_FILE_NAME)
     }
 
+    /// The path to the directory where direct trust keystore is stored
     pub fn direct_trust_dir_path(&self) -> PathBuf {
         self.root_dir_path().join(DIRECT_TRUST_DIR_NAME)
     }
 
+    /// Get the relative path of the file containing the configuration of the verifications
     pub fn get_verification_list_str(&self) -> &'static str {
         VERIFICATION_LIST
     }
@@ -137,7 +150,7 @@ pub(crate) mod test {
         let c = Config::default();
         assert_eq!(c.root_dir_path(), Path::new("."));
         assert_eq!(c.log_file_path(), Path::new("./log/log.txt"));
-        assert_eq!(c.direct_trust_dir_path(), Path::new("./direct_trust"));
+        assert_eq!(c.direct_trust_dir_path(), Path::new("./direct-trust"));
         assert!(!c.get_verification_list_str().is_empty());
     }
 }
