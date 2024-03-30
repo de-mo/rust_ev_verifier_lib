48ccff60f1ce841935b4e34539b4adb6473805b3
De-Mo <denis@morelfr.ch>
2024-03-30 17:12:08 +0000

    Adapt checks with the new directory structure

--- src/application_runner/checks.rs
+++ src/application_runner/checks.rs
@@ -1,14 +1,14 @@
 use std::path::Path;
 
 use crate::{
-    config::Config as VerifierConfig,
+    config::{Config as VerifierConfig, CONTEXT_DIR_NAME, SETUP_DIR_NAME, TALLY_DIR_NAME},
     verification::{meta_data::VerificationMetaDataList, VerificationPeriod},
 };
 use anyhow::{anyhow, ensure};
 
 /// Check some elements at start of the application.
 ///
-/// Must be caled by the application at the beginning. If error, then cannot continue
+/// Must be called by the application at the beginning. If error, then cannot continue
 pub fn start_check(config: &'static VerifierConfig) -> anyhow::Result<()> {
     let md_list_check = VerificationMetaDataList::load(config.get_verification_list_str());
     ensure!(
@@ -24,56 +24,42 @@ pub fn start_check(config: &'static VerifierConfig) -> anyhow::Result<()> {
     Ok(())
 }
 
-pub fn is_directory_tally(path: &Path) -> anyhow::Result<bool> {
+pub fn check_verification_dir(period: &VerificationPeriod, path: &Path) -> anyhow::Result<()> {
     ensure!(
         path.is_dir(),
-        format!("Giveen directory {:?} does not exist", path)
+        format!("Given directory {:?} does not exist", path)
     );
     ensure!(
-        path.join(VerifierConfig::setup_dir_name()).is_dir(),
+        path.join(CONTEXT_DIR_NAME).is_dir(),
         format!(
-            "The setup directory {:?} does not exist",
-            path.join(VerifierConfig::setup_dir_name())
+            "Directory {} does not exist in directory {:?}",
+            CONTEXT_DIR_NAME, path
         )
     );
-    Ok(path.join(VerifierConfig::tally_dir_name()).is_dir())
-}
-
-pub fn check_verification_dir(period: &VerificationPeriod, path: &Path) -> anyhow::Result<()> {
-    let is_tally = is_directory_tally(path)?;
-    match period.is_tally() {
-        true => match is_tally {
-            true => Ok(()),
-            false => Err(anyhow!(format!(
-                "The tally directory {:?} does not exist",
-                path.join(VerifierConfig::tally_dir_name())
-            ))),
-        },
-        false => Ok(()),
+    match period {
+        VerificationPeriod::Setup => ensure!(
+            path.join(SETUP_DIR_NAME).is_dir(),
+            format!(
+                "Directory {} does not exist in directory {:?}",
+                SETUP_DIR_NAME, path
+            )
+        ),
+        VerificationPeriod::Tally => ensure!(
+            path.join(TALLY_DIR_NAME).is_dir(),
+            format!(
+                "Directory {} does not exist in directory {:?}",
+                TALLY_DIR_NAME, path
+            )
+        ),
     }
+    Ok(())
 }
 
 #[cfg(test)]
 mod test {
     use super::{VerificationPeriod, *};
     use crate::config::test::test_datasets_path;
-    use std::path::{Path, PathBuf};
-
-    pub(crate) fn dataset_setup_path() -> PathBuf {
-        test_datasets_path().join("dataset-setup")
-    }
-
-    pub(crate) fn dataset_tally_path() -> PathBuf {
-        test_datasets_path().join("dataset-tally")
-    }
-
-    #[test]
-    fn test_is_directory_tally() {
-        assert!(is_directory_tally(Path::new("./toto")).is_err());
-        assert!(is_directory_tally(Path::new(".")).is_err());
-        assert!(!is_directory_tally(&dataset_setup_path()).unwrap());
-        assert!(is_directory_tally(&dataset_tally_path()).unwrap());
-    }
+    use std::path::Path;
 
     #[test]
     fn test_check_verification_dir() {
@@ -81,9 +67,7 @@ mod test {
         assert!(check_verification_dir(&VerificationPeriod::Tally, Path::new("./toto")).is_err());
         assert!(check_verification_dir(&VerificationPeriod::Setup, Path::new(".")).is_err());
         assert!(check_verification_dir(&VerificationPeriod::Tally, Path::new(".")).is_err());
-        assert!(check_verification_dir(&VerificationPeriod::Setup, &dataset_setup_path()).is_ok());
-        assert!(check_verification_dir(&VerificationPeriod::Tally, &dataset_setup_path()).is_err());
-        assert!(check_verification_dir(&VerificationPeriod::Setup, &dataset_tally_path()).is_ok());
-        assert!(check_verification_dir(&VerificationPeriod::Tally, &dataset_tally_path()).is_ok());
+        assert!(check_verification_dir(&VerificationPeriod::Setup, &test_datasets_path()).is_ok());
+        assert!(check_verification_dir(&VerificationPeriod::Tally, &test_datasets_path()).is_ok());
     }
 }
