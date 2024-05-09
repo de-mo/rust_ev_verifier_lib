fd11585f7db864809c0af92630cf82f7ee27c8c5
De-Mo <denis@morelfr.ch>
2024-05-09 12:49:46 +0000

    Verification 03.11

--- src/verification/setup/consistency/mod.rs
+++ src/verification/setup/consistency/mod.rs
@@ -8,6 +8,7 @@ mod v0307_election_pk_consistency;
 mod v0308_primes_mapping_table_consistency;
 mod v0309_election_event_id_consistency;
 mod v0310_card_set_ids_consistency;
+mod v0311_file_name_verification_card_set_ids_consistency;
 mod v0313_total_voters_consistency;
 mod v0315_chunk_consistency;
 
@@ -96,7 +97,7 @@ pub fn get_verifications<'a>(
         Verification::new(
             "03.11",
             "VerifyFileNameVerificationCardSetIdsConsistency",
-            verification_unimplemented,
+            v0311_file_name_verification_card_set_ids_consistency::fn_verification,
             metadata_list,
             config,
         )?,
---
+++ src/verification/setup/consistency/v0311_file_name_verification_card_set_ids_consistency.rs
@@ -0,0 +1,144 @@
+use super::super::super::result::{
+    create_verification_error, create_verification_failure, VerificationEvent, VerificationResult,
+};
+use crate::{
+    config::Config,
+    file_structure::{
+        context_directory::ContextDirectoryTrait, setup_directory::SetupDirectoryTrait,
+        VerificationDirectoryTrait,
+    },
+};
+use anyhow::anyhow;
+use log::debug;
+
+fn verify_file_name_correct(vcs_ids: &[&str], dir_names: &[String]) -> VerificationResult {
+    let mut res = VerificationResult::new();
+    let mut vcs_ids_ordered = vcs_ids.to_vec();
+    vcs_ids_ordered.sort();
+    let mut dir_names_ordered = dir_names.to_vec();
+    dir_names_ordered.sort();
+    if vcs_ids_ordered != dir_names_ordered {
+        res.push(create_verification_failure!(format!(
+            "The subdirectory [{}] are not equal to the list of voting card set ids [{}]",
+            dir_names_ordered.join(","),
+            vcs_ids_ordered.join(",")
+        )))
+    }
+    res
+}
+
+pub(super) fn fn_verification<D: VerificationDirectoryTrait>(
+    dir: &D,
+    _config: &'static Config,
+    result: &mut VerificationResult,
+) {
+    let context_dir = dir.context();
+    let setup_dir = dir.unwrap_setup();
+
+    let ee_context = match context_dir.election_event_context_payload() {
+        Ok(payload) => payload,
+        Err(e) => {
+            result.push(create_verification_error!(
+                "Cannot read payload for election_event_context_payload",
+                e
+            ));
+            return;
+        }
+    };
+    let vcs_ids = ee_context.election_event_context.vcs_ids();
+
+    result.append_wtih_context(
+        &verify_file_name_correct(&vcs_ids, &context_dir.vcs_directory_names()),
+        "Context directory",
+    );
+
+    result.append_wtih_context(
+        &verify_file_name_correct(&vcs_ids, &setup_dir.vcs_directory_names()),
+        "Setup directory",
+    );
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::config::test::{
+        get_test_verifier_mock_setup_dir as get_mock_verifier_dir,
+        get_test_verifier_setup_dir as get_verifier_dir, CONFIG_TEST,
+    };
+
+    #[test]
+    fn test_ok() {
+        let dir = get_verifier_dir();
+        let mut result = VerificationResult::new();
+        fn_verification(&dir, &CONFIG_TEST, &mut result);
+        if !result.is_ok() {
+            for e in result.errors() {
+                println!("{}", e);
+            }
+            for f in result.failures() {
+                println!("{}", f);
+            }
+        }
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_nok_add_vcs() {
+        let mut dir = get_mock_verifier_dir();
+        let mut election_event_context_payload =
+            dir.context().election_event_context_payload().unwrap();
+        let mut context = election_event_context_payload
+            .as_ref()
+            .election_event_context
+            .verification_card_set_contexts[0]
+            .clone();
+        context.verification_card_set_id = "toto".to_string();
+        election_event_context_payload
+            .as_mut()
+            .election_event_context
+            .verification_card_set_contexts
+            .push(context);
+        dir.context_mut()
+            .mock_election_event_context_payload(&Ok(&election_event_context_payload));
+        let mut result = VerificationResult::new();
+        fn_verification(&dir, &CONFIG_TEST, &mut result);
+        assert!(result.has_failures());
+        assert!(!result.has_errors());
+    }
+
+    #[test]
+    fn test_nok_change_vcs_id() {
+        let mut dir = get_mock_verifier_dir();
+        let mut election_event_context_payload =
+            dir.context().election_event_context_payload().unwrap();
+        election_event_context_payload
+            .as_mut()
+            .election_event_context
+            .verification_card_set_contexts[0]
+            .verification_card_set_id = "toto".to_string();
+        dir.context_mut()
+            .mock_election_event_context_payload(&Ok(&election_event_context_payload));
+        let mut result = VerificationResult::new();
+        fn_verification(&dir, &CONFIG_TEST, &mut result);
+        assert!(result.has_failures());
+        assert!(!result.has_errors());
+    }
+
+    #[test]
+    fn test_nok_change_remove_vcs() {
+        let mut dir = get_mock_verifier_dir();
+        let mut election_event_context_payload =
+            dir.context().election_event_context_payload().unwrap();
+        election_event_context_payload
+            .as_mut()
+            .election_event_context
+            .verification_card_set_contexts
+            .pop();
+        dir.context_mut()
+            .mock_election_event_context_payload(&Ok(&election_event_context_payload));
+        let mut result = VerificationResult::new();
+        fn_verification(&dir, &CONFIG_TEST, &mut result);
+        assert!(result.has_failures());
+        assert!(!result.has_errors());
+    }
+}
