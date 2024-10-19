b9e9dfd0f3bf00ea35947ff319a4041ac2c761a9
Mona <test@test.ch>
2024-10-19 17:12:24 +0000

    Implement 03.08

---
+++ src/verification/setup/consistency/v0308_primes_mapping_table_consistency/consistent_xml.rs
@@ -0,0 +1,354 @@
+use rust_ev_system_library::preliminaries::PTableElement;
+
+use crate::{
+    data_structures::{
+        context::{
+            election_event_configuration::{
+                Answer, Candidate, Election, ElectionInformation, List, StandardQuestion, Vote,
+                WriteInCandidate,
+            },
+            election_event_context_payload::ElectionEventContext,
+        },
+        ElectionEventConfiguration,
+    },
+    verification::{VerificationEvent, VerificationResult},
+};
+use std::{collections::HashMap, slice::Iter};
+
+pub fn verification_2_3_same_than_xml(
+    ee_context: &ElectionEventContext,
+    ee_config: &ElectionEventConfiguration,
+) -> VerificationResult {
+    let mut result = VerificationResult::new();
+    for vcs_context in &ee_context.verification_card_set_contexts {
+        let p_table_calculated = match generate_all_p_table_elements(
+            &vcs_context.verification_card_set_alias,
+            ee_config,
+        ) {
+            Ok(r) => r
+                .iter()
+                .map(|e| (e.actual_voting_option.clone(), e.clone()))
+                .collect::<HashMap<_, _>>(),
+            Err(e) => {
+                result.push(VerificationEvent::new_error(&e).add_context(format!(
+                    "calculating p_table for vcs_id {}",
+                    vcs_context.verification_card_set_id
+                )));
+                continue;
+            }
+        };
+        for p_table_element in &vcs_context.primes_mapping_table.p_table {
+            let p_table_element_without_encoded =
+                PTableElementWithoutEncoding::from(p_table_element);
+            match p_table_calculated.get(&p_table_element.actual_voting_option) {
+                Some(elt) => {
+                    if elt != &p_table_element_without_encoded {
+                        result.push(
+                        VerificationEvent::new_failure(&format!(
+                            "PTable Element not the same for the actual voting option {}\n Original: {:?} \n Calculated: {:?}",
+                            &p_table_element.actual_voting_option, &p_table_element_without_encoded, elt
+                        ))
+                        .add_context(format!(
+                            "Verification 2 for vcs_id {}",
+                            vcs_context.verification_card_set_id
+                        )),
+                    )
+                    }
+                }
+                None => result.push(
+                    VerificationEvent::new_failure(&format!(
+                        "voting option {} not found in xml",
+                        &p_table_element.actual_voting_option
+                    ))
+                    .add_context(format!(
+                        "Verification 2 for vcs_id {}",
+                        vcs_context.verification_card_set_id
+                    )),
+                ),
+            }
+        }
+        let expected_count = p_table_calculated.len();
+        let p_table_count = vcs_context.primes_mapping_table.p_table.len();
+        if expected_count != p_table_count {
+            result.push(
+            VerificationEvent::new_failure(&format!(
+                "The number of entries in the p_table (={}) is not the same than the number of entries calculated (={})",
+                p_table_count, expected_count
+            ))
+            .add_context(format!(
+                "Verification 2 for vcs_id {}",
+                vcs_context.verification_card_set_id
+            )),
+        )
+        }
+    }
+    result
+}
+
+/// Element in pTable according the spefication of Swiss Post
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct PTableElementWithoutEncoding {
+    pub actual_voting_option: String,
+    pub semantic_information: String,
+    pub correctness_information: String,
+}
+
+impl From<&PTableElement> for PTableElementWithoutEncoding {
+    fn from(value: &PTableElement) -> Self {
+        Self {
+            actual_voting_option: value.actual_voting_option.clone(),
+            semantic_information: value.semantic_information.clone(),
+            correctness_information: value.correctness_information.clone(),
+        }
+    }
+}
+
+macro_rules! generate_text_4_languages {
+    ($elt: expr, $l: ident, $f: ident) => {
+        ["de", "fr", "it", "rm"]
+            .iter()
+            .map(|&l| match $elt.iter().find(|v| v.$l.as_str() == l) {
+                Some(v) => v.$f.clone(),
+                None => String::default(),
+            })
+            .filter(|s| !s.is_empty())
+            .collect::<Vec<_>>()
+            .join("|")
+            .as_str()
+    };
+}
+
+fn generate_all_p_table_elements(
+    vcs_alias: &str,
+    ee_config: &ElectionEventConfiguration,
+) -> Result<Vec<PTableElementWithoutEncoding>, String> {
+    let auth_id = vcs_alias.chars().skip(4).collect::<String>();
+    let auth = match ee_config
+        .authorizations
+        .iter()
+        .map_err(|e| format!("Error getting authorization {}: {}", &auth_id, e))?
+        .find(|a| a.authorization_identification == auth_id)
+    {
+        Some(a) => a,
+        None => return Err(format!("Authorization id {auth_id} not found")),
+    };
+    let mut res = vec![];
+    res.append(
+        &mut ee_config
+            .contest
+            .votes
+            .iter()
+            .map_err(|e| format!("Error iterating votes: {}", e))?
+            .filter(|v| v.has_authorization(&auth))
+            .flat_map(|v| generate_p_table_for_vote(&v.vote))
+            .collect(),
+    );
+    res.append(
+        &mut ee_config
+            .contest
+            .election_groups
+            .iter()
+            .map_err(|e| format!("Error iterating election groups: {}", e))?
+            .filter(|v| v.has_authorization(&auth))
+            .flat_map(|eg| {
+                eg.election_information
+                    .iter()
+                    .flat_map(generate_p_table_for_election_information)
+                    .collect::<Vec<_>>()
+            })
+            .collect(),
+    );
+    Ok(res)
+}
+
+fn generate_p_table_element_for_answer(
+    answer: &Answer,
+    question: &StandardQuestion,
+) -> PTableElementWithoutEncoding {
+    PTableElementWithoutEncoding {
+        actual_voting_option: format!(
+            "{}|{}",
+            question.question_identification, answer.answer_identification
+        ),
+        semantic_information: [
+            match answer.hidden_answer {
+                Some(true) => "BLANK",
+                _ => "NON_BLANK",
+            },
+            generate_text_4_languages!(
+                question.ballot_question.ballot_question_info.as_slice(),
+                language,
+                ballot_question
+            ),
+            generate_text_4_languages!(answer.answer_info.as_slice(), language, answer),
+        ]
+        .join("|"),
+        correctness_information: question.question_identification.clone(),
+    }
+}
+
+fn generate_p_table_for_question(question: &StandardQuestion) -> Vec<PTableElementWithoutEncoding> {
+    question
+        .answer
+        .iter()
+        .map(|answer| generate_p_table_element_for_answer(answer, question))
+        .collect()
+}
+
+fn generate_p_table_for_vote(vote: &Vote) -> Vec<PTableElementWithoutEncoding> {
+    let mut res = vec![];
+    for ballot in vote.ballot.iter() {
+        if let Some(standard_ballot) = &ballot.standard_ballot {
+            res.append(&mut generate_p_table_for_question(standard_ballot));
+        }
+        if let Some(variant_ballot) = &ballot.variant_ballot {
+            res.append(
+                &mut variant_ballot
+                    .standard_question
+                    .iter()
+                    .flat_map(generate_p_table_for_question)
+                    .collect(),
+            );
+            res.append(
+                &mut variant_ballot
+                    .tie_break_question
+                    .iter()
+                    .flat_map(generate_p_table_for_question)
+                    .collect(),
+            );
+        }
+    }
+    res
+}
+
+fn generate_p_table_for_election_information(
+    election_info: &ElectionInformation,
+) -> Vec<PTableElementWithoutEncoding> {
+    let mut res = vec![];
+    // lists, only if proportional election
+    if election_info.election.type_of_election == 1 {
+        res.append(
+            &mut election_info
+                .list
+                .iter()
+                .map(|l| generate_p_table_element_for_list(l, &election_info.election))
+                .collect::<Vec<_>>(),
+        );
+    }
+    // candidates
+    res.append(
+        &mut election_info
+            .candidate
+            .iter()
+            .flat_map(|c| generate_p_table_element_for_candidate(c, &election_info.election))
+            .collect::<Vec<_>>(),
+    );
+    // write in candidates
+    if let Some(write_in_candidates) = &election_info.write_in_candidate {
+        res.append(&mut generate_p_table_element_for_write_in_candidate(
+            write_in_candidates.as_slice(),
+            &election_info.election,
+        ));
+    }
+    // Empty positions
+    res.append(&mut generate_p_table_element_for_blank_position_candidate(
+        &mut election_info.list.iter(),
+        &election_info.election,
+    ));
+    res
+}
+
+fn generate_p_table_element_for_list(
+    list: &List,
+    election: &Election,
+) -> PTableElementWithoutEncoding {
+    PTableElementWithoutEncoding {
+        actual_voting_option: format!(
+            "{}|{}",
+            election.election_identification, list.list_identification
+        ),
+        semantic_information: [
+            match list.list_empty {
+                true => "BLANK",
+                _ => "NON_BLANK",
+            },
+            generate_text_4_languages!(
+                list.list_description.list_description_info.as_slice(),
+                language,
+                list_description
+            ),
+        ]
+        .join("|"),
+        correctness_information: format!("L|{}", election.election_identification),
+    }
+}
+
+fn generate_p_table_element_for_candidate(
+    candidate: &Candidate,
+    election: &Election,
+) -> Vec<PTableElementWithoutEncoding> {
+    (0..election.candidate_accumulation)
+        .map(|i| PTableElementWithoutEncoding {
+            actual_voting_option: format!(
+                "{}|{}|{}",
+                election.election_identification, candidate.candidate_identification, i
+            ),
+            semantic_information: [
+                "NON_BLANK",
+                &candidate.family_name,
+                &candidate.first_name,
+                &candidate.call_name,
+                &candidate.date_of_birth,
+            ]
+            .join("|"),
+            correctness_information: format!("C|{}", election.election_identification),
+        })
+        .collect()
+}
+
+fn generate_p_table_element_for_write_in_candidate(
+    candidates: &[WriteInCandidate],
+    election: &Election,
+) -> Vec<PTableElementWithoutEncoding> {
+    candidates
+        .iter()
+        .map(|c| PTableElementWithoutEncoding {
+            actual_voting_option: format!(
+                "{}|{}",
+                election.election_identification, c.write_in_candidate_identification
+            ),
+            semantic_information: [
+                "WRITE_IN",
+                format!("WRITE_IN_POSITION-{}", c.position).as_str(),
+            ]
+            .join("|"),
+            correctness_information: format!("C|{}", election.election_identification),
+        })
+        .collect()
+}
+
+fn generate_p_table_element_for_blank_position_candidate(
+    it_lists: &mut Iter<'_, List>,
+    election: &Election,
+) -> Vec<PTableElementWithoutEncoding> {
+    it_lists
+        .filter(|l| l.list_empty)
+        .flat_map(|l| {
+            l.candidate_position
+                .iter()
+                .map(|c| PTableElementWithoutEncoding {
+                    actual_voting_option: format!(
+                        "{}|{}",
+                        election.election_identification, c.candidate_list_identification
+                    ),
+                    semantic_information: [
+                        "BLANK",
+                        format!("EMPTY_CANDIDATE_POSITION-{}", c.position_on_list).as_str(),
+                    ]
+                    .join("|"),
+                    correctness_information: format!("C|{}", election.election_identification),
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect()
+}
---
+++ src/verification/setup/consistency/v0308_primes_mapping_table_consistency/mod.rs
@@ -0,0 +1,103 @@
+mod consistent_xml;
+
+use super::super::super::result::{VerificationEvent, VerificationResult};
+use crate::{
+    config::Config,
+    data_structures::context::election_event_context_payload::ElectionEventContext,
+    file_structure::{context_directory::ContextDirectoryTrait, VerificationDirectoryTrait},
+};
+use consistent_xml::verification_2_3_same_than_xml;
+use rust_ev_system_library::preliminaries::PTableElement;
+use std::collections::HashMap;
+
+pub(super) fn fn_verification<D: VerificationDirectoryTrait>(
+    dir: &D,
+    _config: &'static Config,
+    result: &mut VerificationResult,
+) {
+    let context_dir = dir.context();
+    let ee_c_paylod = match context_dir.election_event_context_payload() {
+        Ok(o) => o,
+        Err(e) => {
+            result.push(
+                VerificationEvent::new_error(&e)
+                    .add_context("Cannot extract election_event_context_payload"),
+            );
+            return;
+        }
+    };
+
+    let ee_configuration = match context_dir.election_event_configuration() {
+        Ok(o) => o,
+        Err(e) => {
+            result.push(
+                VerificationEvent::new_error(&e)
+                    .add_context("Cannot extract election_event_configuration"),
+            );
+            return;
+        }
+    };
+
+    // Verification 1
+    result.append_with_context(
+        &verify_1_same_actual_voting_options(&ee_c_paylod.election_event_context),
+        "Verification 1 (same actual voting option v_i maps to the same element of the pTable)",
+    );
+
+    // Verifications 2 and 3
+    result.append_with_context(
+        &verification_2_3_same_than_xml(&ee_c_paylod.election_event_context, &ee_configuration),
+        "Verification 2 and 3 (consistent to xml)",
+    );
+}
+
+/// Verification 1 according to the specification of Swiss Post
+/// The same actual voting option v_i maps to the same element of the pTable
+fn verify_1_same_actual_voting_options(ee_context: &ElectionEventContext) -> VerificationResult {
+    let mut result = VerificationResult::new();
+    let mut actual_hashmaps: HashMap<String, &PTableElement> = HashMap::new();
+    for vcs_context in ee_context.verification_card_set_contexts.iter() {
+        let vcs_id = vcs_context.verification_card_set_id.as_str();
+        for p_table_element in vcs_context.primes_mapping_table.p_table.iter() {
+            match actual_hashmaps.get(&p_table_element.actual_voting_option) {
+                Some(found_p_table_element) => {
+                    if found_p_table_element != &p_table_element {
+                        result.push(VerificationEvent::new_failure(&format!(
+                            "The actual voting option {} for vcs_id{} is not the same as the previous one\n Found: {}\n Expected: {}",
+                            p_table_element.actual_voting_option, vcs_id, found_p_table_element, p_table_element
+                        )));
+                    }
+                }
+                None => {
+                    let _ = actual_hashmaps.insert(
+                        p_table_element.actual_voting_option.clone(),
+                        p_table_element,
+                    );
+                }
+            };
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::config::test::{get_test_verifier_setup_dir as get_verifier_dir, CONFIG_TEST};
+
+    #[test]
+    fn test_ok() {
+        let dir = get_verifier_dir();
+        let mut result = VerificationResult::new();
+        fn_verification(&dir, &CONFIG_TEST, &mut result);
+        if !result.is_ok() {
+            for r in result.errors_to_string() {
+                println!("{:?}", r)
+            }
+            for r in result.failures_to_string() {
+                println!("{:?}", r)
+            }
+        }
+        assert!(result.is_ok());
+    }
+}
