ae3fce3918be1245f6c37d953e6eb6f123871531
Denis <denis@morelfr.ch>
2024-11-16 17:21:22 +0000

    Verification 0805

--- src/verification/README.md
+++ src/verification/README.md
@@ -50,7 +50,7 @@ The following table gives the situation of the development of the verifications 
 | Tally | 08.02        | Implemented     | Done (without negative) |
 | Tally | 08.03        | Implemented     | Done (without negative) |
 | Tally | 08.04        | Implemented     | Done (without negative) |
-| Tally | 08.05        | Not implemented
+| Tally | 08.05        | Implemented     | Done (without negative) |
 | Tally | 08.06        | Not implemented
 | Tally | 08.07        | Not implemented
 | Tally | 08.08        | Not implemented
---
+++ src/verification/tally/consistency/v0805_verify_ballot_box_ids_consistency.rs
@@ -0,0 +1,90 @@
+use super::super::super::result::{VerificationEvent, VerificationResult};
+use crate::{
+    config::Config,
+    file_structure::{
+        tally_directory::BBDirectoryTrait, TallyDirectoryTrait, VerificationDirectoryTrait,
+    },
+};
+
+pub(super) fn fn_verification<D: VerificationDirectoryTrait>(
+    dir: &D,
+    _config: &'static Config,
+    result: &mut VerificationResult,
+) {
+    let tally_dir = dir.unwrap_tally();
+
+    for bb_dir in tally_dir.bb_directories().iter() {
+        let bb_id = bb_dir.name();
+
+        for (i, cc_bb_payload_res) in bb_dir.control_component_ballot_box_payload_iter() {
+            match cc_bb_payload_res {
+                Ok(p) => {
+                    if p.ballot_box_id != bb_id {
+                        result.push(VerificationEvent::new_failure(&format!(
+                        "bb_id (={}) in {}/control_component_ballot_box_payload_{} is not the same than the directory",
+                        &p.ballot_box_id, bb_id, i
+                    )));
+                    }
+                }
+                Err(e) => result.push(VerificationEvent::new_error(&e).add_context(format!(
+                    "{}/control_component_ballot_box_payload_{} cannot be read",
+                    bb_id, i
+                ))),
+            }
+        }
+
+        for (i, cc_bb_payload_res) in bb_dir.control_component_shuffle_payload_iter() {
+            match cc_bb_payload_res {
+                Ok(p) => {
+                    if p.ballot_box_id != bb_id {
+                        result.push(VerificationEvent::new_failure(&format!(
+                        "bb_id (={}) in {}/control_component_shuffle_payload_{} is not the same than the directory",
+                        &p.ballot_box_id, bb_id, i
+                    )));
+                    }
+                }
+                Err(e) => result.push(VerificationEvent::new_error(&e).add_context(format!(
+                    "{}/control_component_shuffle_payload_{} cannot be read",
+                    bb_id, i
+                ))),
+            }
+        }
+
+        match bb_dir.tally_component_shuffle_payload() {
+            Ok(p) => {
+                if p.ballot_box_id != bb_id {
+                    result.push(VerificationEvent::new_failure(&format!(
+                    "bb_id (={}) in {}/tally_component_shuffle_payload is not the same than the directory",
+                    &p.ballot_box_id, bb_id
+                )));
+                }
+            }
+            Err(e) => result.push(VerificationEvent::new_error(&e).add_context(format!(
+                "{}/tally_component_shuffle_payload cannot be read",
+                bb_id
+            ))),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::config::test::{get_test_verifier_tally_dir as get_verifier_dir, CONFIG_TEST};
+
+    #[test]
+    fn test_ok() {
+        let dir = get_verifier_dir();
+        let mut result = VerificationResult::new();
+        fn_verification(&dir, &CONFIG_TEST, &mut result);
+        if !result.is_ok() {
+            for r in result.errors_to_string() {
+                println!("{:?}", r)
+            }
+            for r in result.failures_to_string() {
+                println!("{:?}", r)
+            }
+        }
+        assert!(result.is_ok());
+    }
+}
